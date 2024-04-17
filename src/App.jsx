4db561c9c1d934fc428830f69f93bb1bfc6732e3
Sk Sakib <sksakib066@gmail.com>
2024-04-17 22:23:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>Website is under maintenance. Stay tuned.</p>
+    </>
+  )
+}
+
+export default App
